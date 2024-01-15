441d16ed6b721324d444d113627923cddfbaece3
Praiz001 <oshilimpraisek@gmail.com>
2024-01-15 08:23:21 +0100

    added seo config

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,33 @@ import type { Metadata } from 'next'
 import './styles/globals.css'
 
 import { NavBar, Footer } from './components/organisms';
-import { SocialMedia, FloatingBoxes,SpotlightSphere } from './components/atoms';
-
+import { SocialMedia, FloatingBoxes, SpotlightSphere } from './components/atoms';
 
 
 export const metadata: Metadata = {
-  title: 'Oshilim Praise',
-  description: '',
+  title: 'Praise Oshilim Portfolio',
+  description: 'Welcome to my portfolio showcasing my skills and projects. I am a passionate web developer dedicated to creating innovative and user-friendly applications.',
+  keywords: ['web development', 'portfolio', 'JavaScript', 'React', 'Next.js', "Typescript"],
+
+  openGraph: {
+    title: "Praise Oshilim Portfolio",
+    description: 'Welcome to my portfolio showcasing my skills and projects. I am a passionate web developer dedicated to creating innovative and user-friendly applications.',
+    url: 'https://praise-oshilim.vercel.app/',
+    locale: 'en-us',
+    type: 'website',
+    // images: '/path/to/your/cover/image.jpg',
+    siteName: 'Praise Oshilim Portfolio',
+  },
+
+  twitter: {
+    title: "Praise Oshilim Portfolio",
+    description: 'Welcome to my portfolio showcasing my skills and projects. I am a passionate web developer dedicated to creating innovative and user-friendly applications.',
+    // images: '/path/to/your/cover/image.jpg',
+    card: 'summary_large_image',
+    creator: '@praise_kel',
+  },
 }
+
 
 export default function RootLayout({
   children,
